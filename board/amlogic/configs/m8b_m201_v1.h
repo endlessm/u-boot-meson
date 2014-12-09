a3707c2bf8920a7e6eb0cb797e4d099d708fb494
Carlo Caione <carlo@caione.org>
2014-12-09 22:58:45 +0100

    uboot: avoid compiling ARC code
    A specific ARC compiler is now used to compile ARC code for supporting
    the suspend. Disable it by disabling suspend.
    
    Signed-off-by: Carlo Caione <carlo@caione.org>

--- board/amlogic/configs/m8b_m201_v1.h
+++ board/amlogic/configs/m8b_m201_v1.h
@@ -81,7 +81,7 @@
 //#define CONFIG_SARADC 1
 #define CONFIG_EFUSE 1
 //#define CONFIG_MACHID_CHECK 1
-#define CONFIG_CMD_SUSPEND 1
+//#define CONFIG_CMD_SUSPEND 1
 //#define CONFIG_IR_REMOTE 1
 #define CONFIG_IR_REMOTE 1
 #define CONFIG_L2_OFF	 1
@@ -439,7 +439,7 @@
  * power down
  */
 //#define CONFIG_CMD_RUNARC 1 /* runarc */
-#define CONFIG_AML_SUSPEND 1
+//#define CONFIG_AML_SUSPEND 1
 
 #define CONFIG_CMD_LOGO
 
