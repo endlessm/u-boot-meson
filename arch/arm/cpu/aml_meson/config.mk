db8d748d3cc53260108d9ae721ed9788cf82242d
Carlo Caione <carlo@caione.org>
2015-01-01 10:33:42 +0100

    u-boot: switch to -mno-unaligned-access
    This patch solves the 'Enter Exception:0x00000004' error we see in
    u-boot when this is compiled with a cross-compiler different from
    CodeSourcery and we try to play with the USB support (i.e. when we give
    'usb start' command). Actually I'm pretty confident that this solves all
    the other cases we have seen in the past related to the same error (see
    the problem with the 'imgread' command).
    
    The exception in the USB case is triggered when accessing the
    wHubCharacteristics field in the struct usb_hub_descriptor with
    descriptor->wHubCharacteristics in common/usb.c
    
    The struct is __packed so accessing this field goes through an
    unaligned access.
    
    Using unaligned loads is the default for ARMv6, ARMv7-A, ARMv7-R, and
    ARMv7-M, where the hardware handles the unaligned load. Note that
    hardware support for unaligned loads is *considerably* faster than a
    software unaligned handler. If you disable the hardware support for
    unaligned loads (via A-bit/U-bit during reset) then you must either
    provide an unaligned load handler or compile with -mno-unaligned-access.
    
    Apparently for some reason in u-boot this hardware feature is disabled
    so we must tell the compiler to use -mno-unaligned-access.
    
    Funny thing is that the CodeSourcery cross-compiler is the only one
    that by default supports -mno-unaligned-access whereas both the linaro
    cross-compilers arm-none-eabi- and arm-linux-gnueabi- use
    -munaligned-access by default, that's why u-boot behaves correctly only
    when compiled with the CodeSourcery cross-toolchain.
    
    Signed-off-by: Carlo Caione <carlo@caione.org>

--- arch/arm/cpu/aml_meson/config.mk
+++ arch/arm/cpu/aml_meson/config.mk
@@ -1,6 +1,8 @@
 CROSS_COMPILE=arm-none-eabi-
 ARM_CPU=cortex-a9
+PF_NO_UNALIGNED := $(call cc-option, -mno-unaligned-access,)
 PLATFORM_CPPFLAGS += $(call cc-option,-mcpu=cortex-a9  -ffixed-r8 -mno-long-calls  -Wall -fPIC )
+PLATFORM_CPPFLAGS += $(PF_NO_UNALIGNED)
 #USE_PRIVATE_LIBGCC=yes
 TEXT_BASE=$(CONFIG_SYS_TEXT_BASE)
 UCL_TEXT_BASE=$(CONFIG_SYS_UCL_TEXT_BASE)
